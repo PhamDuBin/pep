708551ce6a2c68964edd01c510962dcff2fdc6fb
Vu <Vu@LAPTOP-HCTCS0HD>
2026-01-05 16:08:28 +0700

    fix[#1]: Seperate .ts file into .ts, .html and .scss

--- frontend/src/app/features/buyer/home/pages/home-page/home-page.component.ts
+++ frontend/src/app/features/buyer/home/pages/home-page/home-page.component.ts
@@ -17,38 +17,8 @@ import { ChatInputComponent } from '../../components/chat-input/chat-input.compo
     ModeSelectorComponent,
     ChatInputComponent
   ],
-  template: `
-    <div class="min-h-screen bg-white">
-      <!-- Header -->
-      <app-header></app-header>
-
-      <!-- Side Menu -->
-      <app-side-menu (projectSelected)="onProjectSelected($event)"></app-side-menu>
-
-      <!-- Main Content Area -->
-      <main class="ml-[197px] pt-[89px]">
-        <!-- Tab Navigation -->
-        <app-tab-navigation [tabs]="tabs" (tabChange)="onTabChange($event)"></app-tab-navigation>
-
-        <!-- Content Area -->
-        <div class="flex flex-col justify-between items-center px-[25px] py-[10px] min-h-[calc(100vh-89px-94px)]">
-          <!-- Center Content -->
-          <div class="flex flex-col items-center justify-center flex-1 gap-[20px] w-full max-w-[800px] mx-auto">
-            <!-- Mode Selector -->
-            <app-mode-selector></app-mode-selector>
-
-            <!-- Chat Input -->
-            <app-chat-input (messageSent)="onMessageSent($event)"></app-chat-input>
-          </div>
-        </div>
-      </main>
-    </div>
-  `,
-  styles: [`
-    :host {
-      display: block;
-    }
-  `]
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
   tabs: Tab[] = [
