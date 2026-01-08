bacdfcb2bddb89c6112ad57d245e88a034ccadd6
hoangpd <hoangpd@i-stech.net>
2026-01-08 11:50:18 +0700

    fix icon Kick in tabbar

--- frontend/src/app/features/shared/components/pagination/pagination.component.ts
+++ frontend/src/app/features/shared/components/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, computed, input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -6,7 +6,7 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './pagination.component.html',
-  styleUrl: './pagination.component.scss'
+  styleUrl: './pagination.component.scss',
 })
 export class PaginationComponent {
   // Support both signal and regular inputs
